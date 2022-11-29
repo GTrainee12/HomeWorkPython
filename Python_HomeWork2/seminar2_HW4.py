5b7aada47a1bd771b7b5c3244c211d83c3fdb43d
Trainee <super.sel12@yadnex.ru>
2022-11-29 21:13:17 +0300

    HomeWork2

---
+++ Python_HomeWork2/seminar2_HW4.py
@@ -0,0 +1,19 @@
+from random import randint 
+numbers = []
+for i in range(10):
+    numbers.append(randint (-10,10))
+print(numbers)
+
+def get_numbers(numbers):
+    count =0 
+    for element in numbers:
+        count +=1
+    return count
+print('Номера элементов: ', get_numbers(numbers))
+
+x = int(input('Укажите позицию первого элемента: '))
+y = int(input('Укажите позицию второго элемента: '))
+
+for i in range(len(numbers)):
+    mult = numbers[x -1]*numbers[y - 1]
+print(f'Множество элементов: {numbers[x -1]} * {numbers[y -1]} =', mult)
